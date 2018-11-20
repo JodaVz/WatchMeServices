e7fa6c976b35a99971480ac25194001660dddf30
Marko <jodavzvz@gmail.com>
2018-11-20 22:40:27 +0100

    Dodavanje veze na bazu

--- WatchMeServices/App_Start/WebApiConfig.cs
+++ WatchMeServices/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -7,6 +9,12 @@ namespace WatchMeServices
 {
     public static class WebApiConfig
     {
+        public static MySqlConnection Conn()
+        {
+            string conn_string = "Server=tcp:air2018watchme.database.windows.net,1433;Initial Catalog=AIR2018WatchMe;Persist Security Info=False;User ID={larisa.borovec@gmail.com};Password={Something24};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            MySqlConnection conn = new MySqlConnection(conn_string);
+            return conn;
+        }
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -20,5 +28,6 @@ namespace WatchMeServices
                 defaults: new { id = RouteParameter.Optional }
             );
         }
+
     }
 }
--- WatchMeServices/Controllers/UsersController.cs
+++ WatchMeServices/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WatchMeServices.Models;
+using MySql.Data.MySqlClient;
 
 namespace WatchMeServices.Controllers
 {
