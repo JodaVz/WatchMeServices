bc6852e51496b581581d3e146ce4a265bd9e4ce6
Marko <jodavzvz@gmail.com>
2018-12-03 21:46:16 +0100

    Dodana funkcionalnost provere postojanja korisnika

--- WatchMeServices/Controllers/UsersController.cs
+++ WatchMeServices/Controllers/UsersController.cs
@@ -14,8 +14,9 @@ namespace WatchMeServices.Controllers
     [RoutePrefix("api/korisnici")]
     public class UsersController : ApiController
     {
-        
 
+        public static string Email;
+        public static string Password;
 
 
         // GET: api/Users/5
@@ -148,25 +149,84 @@ namespace WatchMeServices.Controllers
         /// <param name="tekst"></param>
         /// treba dodati rad s bazom, i insert
         // POST: api/Users
-        public void Post([FromBody]string tekst)
+        //public void Post([FromBody]string tekst)
+        //{
+        //    try
+        //    {
+        //        string proba = tekst;
+        //        Users korisnik = JsonConvert.DeserializeObject<Users>(tekst);
+        //        int id = korisnik.ID;
+        //        string ime = korisnik.Name;
+        //        string prezime = korisnik.Surname;
+        //        string email = korisnik.email;
+        //        string password = korisnik.Password;
+               
+        //    }
+        //    catch (Exception e)
+        //    {
+
+        //        throw e;
+        //    }
+           
+        //}
+
+        [HttpPost]
+        [Route("users/provjeri")]
+        public void SaveName([FromBody]string UsrPass)
         {
             try
             {
-                string proba = tekst;
-                Users korisnik = JsonConvert.DeserializeObject<Users>(tekst);
-                int id = korisnik.ID;
-                string ime = korisnik.Name;
-                string prezime = korisnik.Surname;
-                string email = korisnik.email;
-                string password = korisnik.Password;
-               
+                string dobivenString = UsrPass;
+                Users user = JsonConvert.DeserializeObject<Users>(dobivenString);
+                string email = user.email;
+                string pass = user.Password;
+                Email = email;
+                Password = pass;
             }
             catch (Exception e)
             {
 
                 throw e;
             }
-           
+
+        }
+        [HttpGet]
+        [Route("users/provjeri")]
+        public IHttpActionResult ProvjeriKorisnika()
+        {
+            Users user = new Users();
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "air2018watchme.database.windows.net";
+                builder.UserID = "larisa.borovec";
+                builder.Password = "Something24";
+                builder.InitialCatalog = "AIR2018WatchMe";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+
+                    connection.Open();
+
+                    string sql = "SELECT * FROM Users WHERE Email=" + "'" + Email + "'" +  " AND "+ "Password=" + "'" +Password+"'";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (!reader.HasRows) return NotFound();//204 Ne postoji
+                            if (reader.HasRows) return Ok();//100 sve je okej, user postoji 
+                        }
+                    }
+                }
+
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            return null;
+
         }
     }
 }
