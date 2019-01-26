fe6ccbe1883db772b7eb80aa08f745ba56576d6e
Marko <jodavzvz@gmail.com>
2019-01-26 15:18:53 +0100

    Dodani novi kontroler za ocjenjivanje filmova, implementirane metode za spremanje ocjene filma

---
+++ WatchMeServices/Controllers/FeedbackController.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WatchMeServices.Models;
+
+namespace WatchMeServices.Controllers
+{
+
+    [RoutePrefix("api/feedback")]
+    public class FeedbackController : ApiController
+    {
+        public static string Comment;
+        public static int LeftBy;
+        public static int CommentedOn;
+        public static int Rating;
+        public SqlConnection connection = Database.DBCon.BuildConnection();
+
+        [HttpPost]
+        [Route("feedback/spremi_ocjenu")]
+        public void SaveMovieFeedback([FromBody]string dobiveniPodaci)
+        {
+            try
+            {
+                string podaci= dobiveniPodaci;
+                Feedback feedback = JsonConvert.DeserializeObject<Feedback>(podaci);
+                //string comment = feedback.Comments;
+                int leftby = feedback.LeftBy;
+                int commentedon = feedback.CommentedOn;
+                int rating = feedback.Rating;
+                //Comment = comment;
+                LeftBy = leftby;
+                CommentedOn = commentedon;
+                Rating = rating;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+        }
+
+        [HttpGet]
+        [Route("feedback/spremi_ocjenu")]
+        public IHttpActionResult DodajRating()
+        {
+
+            try
+            {
+
+
+                using (connection)
+                {
+
+                    connection.Open();
+
+                    string sql = "INSERT INTO Feedback(LeftBy,CommentedOn,Rating) VALUES("+LeftBy+","+CommentedOn+","+Rating+");";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        SqlDataReader reader = command.ExecuteReader();
+                    }
+                    connection.Close();
+                    return Ok();
+                    
+                }
+
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            return null;
+
+        }
+    }
+}
---
+++ WatchMeServices/Models/Feedback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WatchMeServices.Models
+{
+    public class Feedback
+    {
+        public int ID { get; set; }
+        public string Comments { get; set; }
+        public int LeftBy { get; set; }
+        public int CommentedOn { get; set; }
+        public int Rating { get; set; }
+    }
+}
